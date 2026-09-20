a65b343d7cd78334e51b6df127dd4454dbb0165d
Landon Moran <160341702+LandonMoran@users.noreply.github.com>
2026-09-20 12:59:49 -0500

    chs: s rule + n test/phone symbols - 8 of 16 rules (all consonants) through the toolchain

--- native/openevv/lang/chs/delta_rules_shim_chs.c
+++ native/openevv/lang/chs/delta_rules_shim_chs.c
@@ -52,7 +52,7 @@ int32_t chs_apply_chi_q_rules(int32_t a0, int32_t a1)
                           &chs_delta_rules[3], a, 2);
 }
 
-int32_t chs_apply_chi_w_rules(int32_t a0, int32_t a1)
+int32_t chs_apply_chi_s_rules(int32_t a0, int32_t a1)
 {
     int32_t a[2];
 
@@ -62,7 +62,7 @@ int32_t chs_apply_chi_w_rules(int32_t a0, int32_t a1)
                           &chs_delta_rules[4], a, 2);
 }
 
-int32_t chs_apply_chi_y_rules(int32_t a0, int32_t a1)
+int32_t chs_apply_chi_w_rules(int32_t a0, int32_t a1)
 {
     int32_t a[2];
 
@@ -72,7 +72,7 @@ int32_t chs_apply_chi_y_rules(int32_t a0, int32_t a1)
                           &chs_delta_rules[5], a, 2);
 }
 
-int32_t chs_apply_chi_z_rules(int32_t a0, int32_t a1)
+int32_t chs_apply_chi_y_rules(int32_t a0, int32_t a1)
 {
     int32_t a[2];
 
@@ -80,5 +80,15 @@ int32_t chs_apply_chi_z_rules(int32_t a0, int32_t a1)
     a[1] = a1;
     return delta_run_rule(EVV_AT(void *, a0),
                           &chs_delta_rules[6], a, 2);
+}
+
+int32_t chs_apply_chi_z_rules(int32_t a0, int32_t a1)
+{
+    int32_t a[2];
+
+    a[0] = a0;
+    a[1] = a1;
+    return delta_run_rule(EVV_AT(void *, a0),
+                          &chs_delta_rules[7], a, 2);
 }
 
